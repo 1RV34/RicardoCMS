53892a85d5c09ed109938fbc7ab3630562a08933
Ricardo Vermeltfoort <ricardovermeltfoort@gmail.com>
2014-05-06 23:58:25 +0200

    added lib dir & added smarty lib

--- system/config/defines.inc.php
+++ system/config/defines.inc.php
@@ -11,5 +11,6 @@ define('_RC_ROOT_DIR_', realpath(dirname(__FILE__).'/../..'));
 define('_RC_SYSTEM_DIR_', _RC_ROOT_DIR_.'/system');
 
 /* System */
-define('_RC_SYSTEM_CONFIG_DIR_', _RC_SYSTEM_DIR_.'/config');
 define('_RC_SYSTEM_CLASS_DIR_', _RC_SYSTEM_DIR_.'/classes');
+define('_RC_SYSTEM_CONFIG_DIR_', _RC_SYSTEM_DIR_.'/config');
+define('_RC_SYSTEM_LIB_DIR_', _RC_SYSTEM_DIR_.'/libs');
