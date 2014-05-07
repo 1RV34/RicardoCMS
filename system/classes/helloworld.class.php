c10f150f821a3709be8c84e4d1f42c8f3b3d90f2
Ricardo Vermeltfoort <ricardovermeltfoort@gmail.com>
2014-05-08 01:18:54 +0200

    added normalize & jquery libs

--- system/classes/helloworld.class.php
+++ system/classes/helloworld.class.php
@@ -9,8 +9,14 @@ class HelloWorld
 {
 	public function __construct()
 	{
-		$smarty = new Smarty_RicardoCMS;
+		$css = new CSS;
+		$css->add('system/lib/normalize.css');
+		$css->printOut();
+		$javaScript = new JavaScript;
+		$javaScript->add('system/lib/jquery.min.js');
+		$javaScript->printOut();
 		p('Debugging is enabled.');
+		$smarty = new Smarty_RicardoCMS;
 		$smarty->display('content/hello-world.tpl');
 	}
 }
