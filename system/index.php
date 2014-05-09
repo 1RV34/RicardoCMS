95d72ee06552dc0e2237b92f22930c38af4966f0
Ricardo Vermeltfoort <ricardovermeltfoort@gmail.com>
2014-05-10 00:55:37 +0200

    add restrictions to system dir

---
+++ system/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Ricardo Vermeltfoort <ricardovermeltfoort@gmail.com>
+ * @copyright 2014 Ricardo Vermeltfoort
+ * @version 0.1.0
+ */
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
