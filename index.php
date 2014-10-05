6056bd37c291003baeea3e40d6974bf3d52f0e86
Ricardo Vermeltfoort <ricardovermeltfoort@gmail.com>
2014-10-05 03:05:26 +0200

    create basic finder

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
  * @version 0.1.0
  */
 
+define('_RC_APPLICATION_DIR_', dirname(__FILE__).'/application');
 require_once dirname(__FILE__).'/system/init.php';
 new HelloWorld;
---
+++ system/classes/finder.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @author Ricardo Vermeltfoort <ricardovermeltfoort@gmail.com>
+ * @copyright 2014 Ricardo Vermeltfoort
+ * @version 0.1.0
+ */
+
+class Finder
+{
+	private static $_instance;
+	private $dirs;
+
+	public static function getInstance()
+	{
+		if (!self::$_instance)
+		{
+			$dirs = array(
+				_RC_APPLICATION_DIR_,
+				_RC_SYSTEM_DIR_,
+			);
+			self::$_instance = new self($dirs);
+		}
+
+		return self::$_instance;
+	}
+
+	public function __construct($dirs)
+	{
+		$this->dirs = $dirs;
+	}
+
+	public function find($dir, $file)
+	{
+		$path = $dir.'/'.$file;
+
+		foreach ($this->dirs as $searchDir)
+		{
+			$searchPath = $searchDir.'/'.$path;
+
+			if (file_exists($searchPath))
+				return $searchPath;
+		}
+
+		return false;
+	}
+}
