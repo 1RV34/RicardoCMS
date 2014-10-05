694eed703dfbdba77a07842dd89bbc2f91d62e62
Ricardo Vermeltfoort <ricardovermeltfoort@gmail.com>
2014-10-05 03:30:13 +0200

    move autoloader into class

--- system/autoloader.php
+++ system/autoloader.php
@@ -6,53 +6,6 @@
  * @version 0.1.0
  */
 
+require_once dirname(__FILE__).'/classes/autoload.php';
 require_once dirname(__FILE__).'/classes/finder.php';
-spl_autoload_register('rcAutoloader');
-
-function rcAutoloader($className)
-{
-	if (class_exists($className, false))
-		return;
-
-	$config = require _RC_SYSTEM_CONFIG_DIR_.'/autoloader.php';
-
-	if (($pos = strpos($className, '_')) !== false)
-	{
-		$type =      strtolower(substr($className, $pos + 1));
-
-		if ($type == 'interface') // Special
-		{
-			if (interface_exists($className, false))
-				return;
-		}
-
-		$className = substr($className, 0, $pos);
-
-		if (array_key_exists($type, $config->directories))
-		{
-			if (!is_array($config->directories[$type]))
-				$config->directories[$type] = array($config->directories[$type]);
-
-			foreach ($config->directories[$type] as $directory)
-			{
-				if ($fileName = Finder::getInstance()->find($directory, strtolower($className).'.php'))
-				{
-					require_once $fileName;
-					return;
-				}
-			}
-		}
-	}
-
-	if (!is_array($config->directories[$config->default]))
-		$config->directories[$config->default] = array($config->directories[$config->default]);
-
-	foreach ($config->directories[$config->default] as $directory)
-	{
-		if ($fileName = Finder::getInstance()->find($directory, strtolower($className).'.php'))
-		{
-			require_once $fileName;
-			return;
-		}
-	}
-}
+spl_autoload_register(array(Autoload_RicardoCMS::getInstance(), 'load'));
---
+++ system/classes/autoload.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @author Ricardo Vermeltfoort <ricardovermeltfoort@gmail.com>
+ * @copyright 2014 Ricardo Vermeltfoort
+ * @version 0.1.0
+ */
+
+class Autoload_RicardoCMS
+{
+	private static $_instance;
+	private $default;
+	private $directories;
+
+	public static function getInstance()
+	{
+		if (!self::$_instance)
+		{
+			$configFile = Finder::getInstance()->find('config', 'autoloader.php');
+			$config = require $configFile;
+			self::$_instance = new self($config->default, $config->directories);
+		}
+
+		return self::$_instance;
+	}
+
+	public function __construct($default, $directories)
+	{
+		$this->default = $default;
+		$this->directories = $directories;
+	}
+
+	public function load($className)
+	{
+		if (class_exists($className, false))
+			return;
+
+		if (($pos = strpos($className, '_')) !== false)
+		{
+			$type =      strtolower(substr($className, $pos + 1));
+
+			if ($type == 'interface') // Special
+			{
+				if (interface_exists($className, false))
+					return;
+			}
+
+			$className = substr($className, 0, $pos);
+
+			if (array_key_exists($type, $this->directories))
+			{
+				if (!is_array($this->directories[$type]))
+					$this->directories[$type] = array($this->directories[$type]);
+
+				foreach ($this->directories[$type] as $directory)
+				{
+					if ($fileName = Finder::getInstance()->find($directory, strtolower($className).'.php'))
+					{
+						require_once $fileName;
+						return;
+					}
+				}
+			}
+		}
+
+		if (!is_array($this->directories[$this->default]))
+			$this->directories[$this->default] = array($this->directories[$this->default]);
+
+		foreach ($this->directories[$this->default] as $directory)
+		{
+			if ($fileName = Finder::getInstance()->find($directory, strtolower($className).'.php'))
+			{
+				require_once $fileName;
+				return;
+			}
+		}
+	}
+}
